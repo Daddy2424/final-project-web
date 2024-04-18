2918daf921b44af5bd5bdb188e0377631f72531a
Deepak Poly <119725894+Daddy2424@users.noreply.github.com>
2024-04-18 22:17:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const password = document.getElementById("password");
 const profileContainer = document.getElementById("profileContainer");
 const userName = document.getElementById("username");
 const usernameContainer = document.getElementById("usernameContainer");
+const ontario = document.getElementById('ontario');
+const alberta = document.getElementById('alberta');
+const bc = document.getElementById('BC');
+const aboutMe = document.getElementById('aboutMe');
+const h1div = document.getElementById('h1Div');
+const PlaceName = document.getElementById('placeName');
 
 
 async function signupPageDisplay(){
@@ -22,7 +28,7 @@ async function signupPageDisplay(){
   console.log(file);
 
   if(file.locked == false){
-    location.href = 'signup.html';
+    location.href = 'signUp.html';
   }
 }
 
@@ -92,6 +98,18 @@ const britishCities = [
 
 let boxesGenerated = false;
 
+ontario.addEventListener('click', ()=>{
+  ShowingCity(ontarioCities);
+})
+alberta.addEventListener('click', ()=>{
+  ShowingCity(albertaCities);
+})
+bc.addEventListener('click', ()=>{
+  ShowingCity(britishCities);
+})
+
+
+
 function ShowingCity(cities) {
 
   // Get the reference to the city-div element only once outside the loop
@@ -103,15 +121,12 @@ function ShowingCity(cities) {
       
       // Create a container div to hold all the city cards
       let containerDiv1 = document.createElement("div");
-      containerDiv1.className = "row my-container1";
-
-      let containerDiv2 = document.createElement("div");
-      containerDiv2.className = "row my-container2";
+      containerDiv1.className = "createdRow";
 
       for (let i = 0; i < cities.length; i++) {
           // Create a new div for each city and append it to containerDiv
           let cityDiv = document.createElement("div");
-          cityDiv.className = "col city-card shadow-box"; // Add a specific class for styling
+          cityDiv.className = "cityCard"; // Add a specific class for styling
           cityDiv.innerHTML = `
               <a href="property.html?city=${cities[i].name}" target="_blank" class="a-city">
               <img src="${cities[i].image}" class="card-img-city" alt="${cities[i].name}">
@@ -122,23 +137,22 @@ function ShowingCity(cities) {
           `;
           
           // Append the new cityDiv to containerDiv
-          if (i < cities.length /2 ){
+    
               containerDiv1.appendChild(cityDiv);
-          }else {
-              containerDiv2.appendChild(cityDiv);
-          }
+      
           
       }
      
       //clear everything
       cityVar.innerHTML = '';
+      aboutMe.style.display = 'none';
+      PlaceName.textContent = 'Popular Cities';
 
       // Append the containerDiv to cityVar
       cityVar.appendChild(containerDiv1);
-      cityVar.appendChild(containerDiv2);
 
       //add back link
-      provContainer.innerHTML = `<a href="index.html" class="back-div">Back</a>`;
+      provContainer.innerHTML = `<a href="index.html" class="back-div">Back -></a>`;
 
       boxesGenerated = true;
       
